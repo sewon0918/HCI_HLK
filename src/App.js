8fd1df3669ed5a4bc29d48fce87c5a412874a6d7
sewon0918 <52196163+sewon0918@users.noreply.github.com>
2021-05-17 22:49:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import UserInformation from './components/UserInformation';
+import Recommendation from './components/Recommendation'
+
+import hamburger from './images/hamburger.png';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return ( 
+            <div className = "App" >
+                <div id = "logo">
+                    <p className='text'><img id = "icon" src={ hamburger } />
+                     Hello Burger!</p>
+                </div>
+                <div className = 'layout'>
+                    <div className = "chatContainer">
+                        <UserInformation  />
+                        <Recommendation />
+                    </div>
+                    <div className = "cart">
+                    Cart
+                    </div>
+                </div>
+                
+            </div>
+            
+        );
+    }
+    
+}
+
+export default App;
---
+++ src/components/Recommendation/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './index.css';
+import 고추장 from '../../images/1.png';
+import 직화 from '../../images/2.png';
+import { isCompositeComponentWithType } from 'react-dom/test-utils';
+
+class Recommendation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: []};
+    }
+
+    menuClick(){
+        document.getElementById("menu1").style.backgroundColor = 'yellow';
+        // console.log("jj")
+    }
+
+    render(){
+        const isLoggedIn = this.state.isLoggedIn;
+        return(
+            <div>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div id="recommendMenu">
+                    <img id = "menu1" className="menu" src={ 고추장 } onClick={this.menuClick}/>
+                    <img className="menu" src={ 직화 } />
+                    <button id="others"> Go to see other menus </button>
+                </div>
+                <div  className='dialog'>"고추장불고기버거" is selected. Do you want option change?</div>
+                <button> Yes </button><button> No </button>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+
+                
+            </div>
+        )
+    }
+}
+
+export default Recommendation;
