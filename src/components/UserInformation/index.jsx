b42e45d97ec1d9064c59881e34ad4cd97e616e4b
unknown <kksan07@kaist.ac.kr>
2021-05-19 15:19:09 +0900

    settimeout

--- src/components/UserInformation/index.jsx
+++ src/components/UserInformation/index.jsx
@@ -8,8 +8,19 @@ class UserInformation extends React.Component {
     constructor(props) {
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
-        this.state = {isLoggedIn: 0, phone: ""};
+        this.onSkip = this.onSkip.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        // this.componentDidMount2 = this.componentDidMount2.bind(this);
+        this.state = {isLoggedIn: 0, phone: "", show: false, show2: false};
     }
+    componentDidMount(){
+        setTimeout(()=>{
+           this.setState({show: true})
+        },1000)
+        setTimeout(()=>{
+            this.setState({show2: true})
+        },3000) 
+     }
     loginClick() {
         this.setState({isLoggedIn: true});
       }
@@ -22,7 +33,9 @@ class UserInformation extends React.Component {
         const number = document.getElementById("number").value;
         console.log(number);
         this.setState({isLoggedIn: 1, phone: number}); 
-        //setTimeout()
+    }
+    onSkip(){
+        this.setState({isLoggedIn: -1, phone: '-1'}); 
     }
     next(){
         document.getElementById("recommendMenu")
@@ -39,12 +52,12 @@ class UserInformation extends React.Component {
         }
         return(
             <div>
-                <div className = 'dialog'>Welcome to Hello Burger! </div>
-                <div className = 'dialog'>Please type your number </div>
+                {this.state.show && <div className = 'dialog'><h3>Welcome to Hello Burger!</h3></div>}
+                {this.state.show2 && <div className = 'dialog'><h3>Please type your number</h3></div>}
                 <div> 
                     <input id = "number"/> 
                     <button onClick = {this.onSubmit}> Sumbit </button>
-                    <button onClick = {this.onSubmit}> Skip </button>
+                    <button onClick = {this.onSkip}> Skip </button>
                 </div>
                 {recommend}
             </div>
