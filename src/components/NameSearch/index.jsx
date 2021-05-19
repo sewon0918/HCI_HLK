2ef16f067d1df6eb02f37fbda1b1fa6355d56914
suplookie <hsk0306@kaist.ac.kr>
2021-05-19 19:19:35 +0900

    name search

---
+++ src/components/NameSearch/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './index.css';
+import 고추장 from '../../images/1.png';
+import 직화 from '../../images/2.png';
+import ShowBurgers from '../ShowBurgers';
+
+class NameSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: "", submitted: false};
+    }
+
+    menuClick(id){
+        const elements = document.getElementsByClassName("menu");
+
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor="white";
+        }
+        console.log(id)
+        document.getElementById(id).style.backgroundColor = 'yellow';
+    
+        // console.log("jj")
+    }
+    onSubmit() {
+        const name = document.getElementById("name").value;
+        console.log(name);
+        this.setState({name: name, submitted: true}); 
+    }
+
+    render(){
+        const submitted = this.state.submitted;
+        const name = this.state.name;
+        let showResult = null;
+        if (submitted) {
+            showResult = <ShowBurgers name={name}></ShowBurgers>
+        }
+        return(
+            <div>
+                <div  className='dialog'>"이름으로 찾기"를 선택했습니다. </div>
+                <div  className='dialog'>"버거 이름을 아래 칸에 입력하고 확인버튼을 눌러주세요. </div>
+                <div> 
+                    <input id = "name"/> 
+                    <button onClick = {this.onSubmit.bind(this)}> 확인 </button>
+                </div>
+                {showResult}
+                
+            </div>
+        )
+    }
+}
+
+export default NameSearch;
