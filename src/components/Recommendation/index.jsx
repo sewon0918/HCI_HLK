94805480231f1f0b8486988b8d89e9883eab43a8
suplookie <hsk0306@kaist.ac.kr>
2021-05-19 15:20:35 +0900

    pass props

--- src/components/Recommendation/index.jsx
+++ src/components/Recommendation/index.jsx
@@ -9,18 +9,27 @@ class Recommendation extends React.Component {
         this.state = {isLoggedIn: []};
     }
 
-    menuClick(){
-        document.getElementById("menu1").style.backgroundColor = 'yellow';
+    menuClick(id){
+        const elements = document.getElementsByClassName("menu");
+
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor="white";
+        }
+        console.log(id)
+        document.getElementById(id).style.backgroundColor = 'yellow';
+    
         // console.log("jj")
     }
 
     render(){
+        const {phone} = this.props;
+            console.log({phone})
         return(
             <div>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                <div  className='dialog'>{phone}These are the recommendation. Select among these or you can click "Others" button</div>
                 <div id="recommendMenu">
-                    <img id = "menu1" className="menu" src={ 고추장 } onClick={this.menuClick} alt="menu1"/>
-                    <img className="menu" src={ 직화 } alt="menu_class"/>
+                    <img id = "menu1" className="menu" src={ 고추장 } onClick={this.menuClick.bind(this, "menu1")} alt="menu1"/>
+                    <img id = "menu2" className="menu" src={ 직화 } onClick={this.menuClick.bind(this, "menu2")} alt="menu_class"/>
                     <button id="others"> Go to see other menus </button>
                 </div>
                 <div  className='dialog'>"고추장불고기버거" is selected. Do you want option change?</div>
