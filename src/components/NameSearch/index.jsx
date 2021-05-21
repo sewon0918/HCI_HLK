0035c3595ac2ebfcb76077d0f60f921c65ce9c14
suplookie <hsk0306@kaist.ac.kr>
2021-05-21 13:15:00 +0900

    ingredients&namesearch

--- src/components/NameSearch/index.jsx
+++ src/components/NameSearch/index.jsx
@@ -7,8 +7,20 @@ import ShowBurgers from '../ShowBurgers';
 class NameSearch extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {name: "", submitted: false};
+        this.state = {name: "", submitted: false, show: false, show2: false, show3: false};
     }
+    componentDidMount(){
+        setTimeout(()=>{
+           this.setState({show: true})
+        },500)
+        setTimeout(()=>{
+            this.setState({show2: true})
+        },1500) 
+        setTimeout(()=>{
+            this.setState({show3: true})
+        },2500) 
+     }
+
 
     menuClick(id){
         const elements = document.getElementsByClassName("menu");
@@ -18,8 +30,6 @@ class NameSearch extends React.Component {
         }
         console.log(id)
         document.getElementById(id).style.backgroundColor = 'yellow';
-    
-        // console.log("jj")
     }
     onSubmit() {
         const name = document.getElementById("name").value;
@@ -29,19 +39,18 @@ class NameSearch extends React.Component {
 
     render(){
         const submitted = this.state.submitted;
-        const name = this.state.name;
         let showResult = null;
         if (submitted) {
-            showResult = <ShowBurgers name={name}></ShowBurgers>
+            showResult = <ShowBurgers name={this.state.name}></ShowBurgers>
         }
         return(
             <div>
-                <div  className='dialog'>"이름으로 찾기"를 선택했습니다. </div>
-                <div  className='dialog'>"버거 이름을 아래 칸에 입력하고 확인버튼을 눌러주세요. </div>
-                <div> 
+                {this.state.show && <div  className='dialog'>"이름으로 찾기"를 선택했습니다. </div>}
+                {this.state.show2 && <div  className='dialog'>버거 이름을 아래 칸에 입력하고 확인버튼을 눌러주세요. </div>}
+                {this.state.show3 && <div> 
                     <input id = "name"/> 
                     <button onClick = {this.onSubmit.bind(this)}> 확인 </button>
-                </div>
+                </div>}
                 {showResult}
                 
             </div>
--- src/components/ShowBurgers/index.jsx
+++ src/components/ShowBurgers/index.jsx
@@ -7,62 +7,109 @@ import burgers from '../../Data/burger.json'
 class ShowBurgers extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isLoggedIn: []};
+        this.state = {menu: "", results: 0, order: false, show: false, show2: false, show3: false, query:""};
     }
 
+
     menuClick(id){
-        const elements = document.getElementsByClassName("menu");
+        const elements = document.getElementsByClassName("showBurger");
 
         for (var i = 0; i < elements.length; i++) {
-            elements[i].style.border="1px solid black";
+            elements[i].style.border="3px solid white";
         }
         console.log(id)
         document.getElementById(id).style.border = '3px solid red';
+        this.setState({menu: id, order: false}); 
     
         // console.log("jj")
+    }
+    orderMenu() {
+        console.log(this.state.menu);
+        this.setState({order: true});
     }
     results() {
         const {name} = this.props;
         const {ingredient} = this.props;
+        let num = 0;
+        if (this.state.query != name) {
+            this.setState({query: name, show: false, show2: false, show3: false, order: false});
+            setTimeout(()=>{
+               this.setState({show: true})
+            },500)
+            setTimeout(()=>{
+                this.setState({show2: true})
+            },1500) 
+            setTimeout(()=>{
+                this.setState({show3: true})
+            },2500) 
+
+        }
         const burgerlist = burgers.map((key, index) => {
             const burgername = key.name;
             if (name != null) {
-                if (name != "" && burgername.includes(name))
+                if (name != "" && burgername.includes(name)) {
+                    num += 1;
                     return (
-                    <img id={burgername} className="menu" src={ require(`../../Data/Image/burgers/${burgername}.jpg`).default } onClick={this.menuClick.bind(this, burgername)} alt="menu_class"/>);
+                        <div key = {burgername} id = {burgername} className="showBurger" onClick={this.menuClick.bind(this, burgername)}>
+                            <img className="image" src={ require(`../../Data/Image/burgers/${burgername}.jpg`).default } alt="menu_class"/>
+                            <div className="name">{burgername}</div>
+                        </div>);
+                }
                 else return (null);
             }
             else if (ingredient != null) {
-                if (key.ingredient === ingredient)
-                    return (<div className='dialog'>{burgername}</div>);
+                if (key.ingredient === ingredient){
+                    num += 1;
+                    return (
+                        <div key = {burgername} id = {burgername} className="showBurger" onClick={this.menuClick.bind(this, burgername)}>
+                            <img className="image" src={ require(`../../Data/Image/burgers/${burgername}.jpg`).default } alt="menu_class"/>
+                            <div className="name">{burgername}</div>
+                        </div>);
+                }
                 else return (null);
             }
             else {
                 console.log("show all");
-                return (<div className='dialog'>{burgername}</div>);
+                return (
+                    <div key = {burgername} id = {burgername} className="showBurger" onClick={this.menuClick.bind(this, burgername)}>
+                        <img className="image" src={ require(`../../Data/Image/burgers/${burgername}.jpg`).default } alt="menu_class"/>
+                        <div className="name">{burgername}</div>
+                    </div>);
             }
         })
-        return (<div id="recommendMenu">{burgerlist}<button id="others"> Go to see other menus </button></div>)
+        if (name != null && num == 0) 
+            return (<>
+                {this.state.show && <div className="dialog">검색 결과가 없습니다. 입력한 내용을 다시 확인해주세요</div>}
+            </>)
+        if (name != null)
+            return (<>
+                {this.state.show && <div  className='dialog'>"{name}"에 대한 검색 결과입니다. </div>}
+                {this.state.show2 && <div  className='dialog'>원하시는 메뉴를 선택하고 주문하기를 눌러주세요. </div>}
+                {this.state.show3 && <><div id="menuShower">{burgerlist}</div><button onClick={this.orderMenu.bind(this)}>주문하기</button></>}
+            </>)
+        if (ingredient != null) 
+            return (<>
+                {this.state.show && <div  className='dialog'>주재료가 {ingredient}인 햄버거입니다. </div>}
+                {this.state.show2 && <div  className='dialog'>원하시는 메뉴를 선택하고 주문하기를 눌러주세요. </div>}
+                {this.state.show3 && <><div id="menuShower">{burgerlist}</div><button onClick={this.orderMenu.bind(this)}>주문하기</button></>}
+            </>)
+        if (name == null && ingredient == null) return (<>
+                {this.state.show && <div  className='dialog'>"모든 메뉴 보기"를 선택했습니다. </div>}
+                {this.state.show2 && <div  className='dialog'>원하시는 메뉴를 선택하고 주문하기를 눌러주세요. </div>}
+                {this.state.show3 && <><div id="menuShower">{burgerlist}</div><button onClick={this.orderMenu.bind(this)}>주문하기</button></>}
+            </>)
     }
 
     render(){
+        let order = null;
+        if (this.state.order) {
+            order = <div>PLACEHOLDER FOR ORDER {this.state.menu}</div>
+        }
         return(
             <div>
-                {this.results()}
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
-                <div id="recommendMenu">
-                    <img id = "menu1" className="menu" src={ 고추장 } onClick={this.menuClick.bind(this, "menu1")} alt="menu1"/>
-                    <img id = "menu2" className="menu" src={ 직화 } onClick={this.menuClick.bind(this, "menu2")} alt="menu_class"/>
-                    <button id="others"> Go to see other menus </button>
-                </div>
-                <div  className='dialog'>"고추장불고기버거" is selected. Do you want option change?</div>
-                <button> Yes </button><button> No </button>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
-                <div  className='dialog'>These are the recommendation. Select among these or you can click "Others" button</div>
+                {this.results()} 
+                {order}
+
 
                 
             </div>
