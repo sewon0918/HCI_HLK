f4af001b1ca8cb68b803575e031032cdd4f1d205
unknown <kksan07@kaist.ac.kr>
2021-05-25 00:54:59 +0900

    css

--- src/components/ShowBurgers/index.jsx
+++ src/components/ShowBurgers/index.jsx
@@ -140,26 +140,26 @@ class ShowBurgers extends React.Component {
             return (<>
                 {<div  className='dialog' id='answer'>{name}</div>}
                 {this.state.show && <div  className='dialog'>"{name}"에 대한 검색 결과입니다. </div>}
-                {this.state.show2 && <div  className='dialog'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
+                {this.state.show2 && <div  className='dialog_long'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
                 {this.state.show3 && <><div id="menuShower">{burgerlist}</div>{nextButton}</>}
             </>)
         if (ingredient != null) 
             return (<>
                 {<div  className='dialog' id='answer'>{ingredient}</div>}
                 {this.state.show && <div  className='dialog'>주재료가 {ingredient}인 햄버거입니다. </div>}
-                {this.state.show2 && <div  className='dialog'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
+                {this.state.show2 && <div  className='dialog_long'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
                 {this.state.show3 && <><div id="menuShower">{burgerlist}</div>{nextButton}</>}
             </>)
         if (recommend)
             return(<>
                 {<div  className='dialog'>"{phone}" 님의 추천메뉴입니다.</div>}
-                {this.state.show && <div  className='dialog'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
+                {this.state.show && <div  className='dialog_long'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
                 {this.state.show2 && <><div id="menuShower">{burgerlist}</div>{nextButton}</>}
             </>)
         if (name == null && ingredient == null) return (<>
                 {<div  className='dialog' id='answer'>모든 메뉴 보기</div>}
                 {this.state.show && <div  className='dialog'>"모든 메뉴 보기"를 선택했습니다. </div>}
-                {this.state.show2 && <div  className='dialog'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
+                {this.state.show2 && <div  className='dialog_long'>원하시는 메뉴를 선택하고 "다음"을 눌러주세요. </div>}
                 {this.state.show3 && <><div id="menuShower">{burgerlist}</div>{nextButton}</>}
             </>)
     }
@@ -190,8 +190,8 @@ class ShowBurgers extends React.Component {
             ifset=<Payment total_price={this.state.info.singleprice}></Payment>
         }
         if(this.state.order){
-            option=<><div className="dialog">{this.state.menu}를 선택하셨습니다. 옵션을 선택하시겠습니까?</div>
-                    <div className="dialog2"><button className="button" onClick={this.optionyes.bind(this)}>네.</button><button className="button" onClick={this.optionno.bind(this)}>아니요.</button></div></>
+            option=<><div id="dialog_long">{this.state.menu}를 선택하셨습니다. 옵션을 선택하시겠습니까?</div>
+                    <div className="dialog2_short2"><button className="button" onClick={this.optionyes.bind(this)}>네.</button><button className="button" onClick={this.optionno.bind(this)}>아니요.</button></div></>
             console.log(this.state.optionSelect);
             if(this.state.optionSelect > 0){
                 order = <OptionChange name={this.state.menu} patty_count={this.state.info.patty_num} cheeze_count={this.state.info.cheeze} onion_state={this.state.info.onion} tomato_state={this.state.info.tomato} lettuce_state={this.state.info.raddish} sauce_state={this.state.info.sauce} single_price={this.state.info.singleprice} set_price={this.state.info.setprice}></OptionChange>
